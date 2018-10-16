5144fa5f398b214c1c5b1c51817dcdc6acf77fc4
anne-other <anne.c.ince@gmail.com>
2018-10-16 20:16:38 +0100

    setting up top list

---
+++ src/components/TopList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TopList = (props) => {
+  return(
+    <div>
+      <h2>
+        top 20 list
+      </h2>
+    </div>
+  )
+}
+
+export default TopList;
--- src/containers/TopBox.js
+++ src/containers/TopBox.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import TopList from '../components/TopList.js'
 
 class TopBox extends Component {
   render() {
     return (
       <div>
         <h1>Top 20</h1>
+        <TopList/>
       </div>
     )
   }

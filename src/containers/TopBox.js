4a58418649481f01761599fa6b06ed6b6f6328e5
anne-other <anne.c.ince@gmail.com>
2018-10-16 20:08:04 +0100

    setting up top box container

---
+++ src/containers/TopBox.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TopBox extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Top 20</h1>
+      </div>
+    )
+  }
+}
+
+export default TopBox

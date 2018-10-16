7e07486fbf39517670eac643751799a7745c86e8
anne-other <anne.c.ince@gmail.com>
2018-10-16 21:02:10 +0100

    trying to get the api working

--- src/containers/TopBox.js
+++ src/containers/TopBox.js
@@ -2,6 +2,22 @@ import React, {Component} from 'react';
 import TopList from '../components/TopList.js'
 
 class TopBox extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount(){
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json'
+    fetch(url)
+    .then((res) => res.json())
+    .then ((items) => {
+      this.setState({items: items});
+    })
+  }
+
   render() {
     return (
       <div>
